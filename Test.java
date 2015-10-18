9e48c9a185b2421337455ad4c5d27ee4f839b0d4
qilaijun <cnuqlj1990@163.com>
2015-10-18 15:15:59 +0800

    insert filename=test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 public class Test{
 	public static void main(String args[]){
+		int m=0;
+	     for (int i=1;i<10;i++){
+			 for(int j=1;j<=i;j++)
+			 {   
+		           m=i*j;
+				System.out.print(%d,m);
+			 }			 
+		 }
 		System.out.print("Git Demo");
 	}
 }
